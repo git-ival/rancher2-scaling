6020a56261d63caa15117eaaaddfc0fc3df49832
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2021-11-17 16:28:31 -0500

    initial commit

---
+++ harvester-clusters/main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    rancher2 = {
+      source  = "rancher/rancher2"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+    }
+    template = {
+      source  = "hashicorp/template"
+    }
+  }
+}
+
+terraform {
+  backend "local" {
+    path = "rancher.tfstate"
+  }
+}
+
+locals {
+  aws_region          = var.aws_region
+  name                = var.cluster_name
+  cluster_count       = var.cluster_count
+  cluster_instance    = terraform.workspace
+  k3s_cluster_secret  = var.k3s_cluster_secret
+  install_k3s_image   = var.install_k3s_image
+  k3d_version         = var.k3d_version
+  ssh_keys            = var.ssh_keys
+  insecure_flag       = var.insecure_flag
+}
+
+provider "aws" {
+  region  = "${local.aws_region}"
+  profile = "rancher-eng"
+}
+
+provider "rancher2" {
+  api_url   = var.rancher_api_url
+  token_key = var.rancher_token_key
+  insecure  = local.insecure_flag
+}
+
+resource "rancher2_cluster" "k3s" {
+  count       = local.cluster_count
+  name        = "${local.name}-${local.cluster_instance}-${count.index}"
+  description = "TF imported cluster ${local.name}-${local.cluster_instance}-${count.index}"
+  labels      = var.cluster_labels
+}
+
+resource "aws_instance" "k3s-server" {
+  ebs_optimized = true
+  instance_type = var.server_instance_type
+  ami           = data.aws_ami.ubuntu.id
+  security_groups = var.security_groups
+  # spot_price           = "1.591"
+  # wait_for_fulfillment = true
+  # spot_type            = "one-time"
+  user_data = templatefile("${path.module}/files/server_userdata.tmpl",
+    {
+      cluster_count         = local.cluster_count,
+      k3s_cluster_secret    = local.k3s_cluster_secret,
+      install_k3s_image     = local.install_k3s_image,
+      k3d_version           = local.k3d_version,
+      k3s_server_args       = var.k3s_server_args,
+      registration_commands = local.insecure_flag ? rancher2_cluster.k3s[*].cluster_registration_token[0].insecure_command : rancher2_cluster.k3s[*].cluster_registration_token[0].command,
+      ssh_keys              = local.ssh_keys
+    }
+  )
+
+  tags = {
+    Name           = "${local.name}-server-${local.cluster_instance}"
+    RancherScaling = var.cluster_name
+    Owner          = var.cluster_name
+  }
+
+  root_block_device {
+    volume_size = "32"
+    volume_type = "gp2"
+  }
+
+  depends_on = [rancher2_cluster.k3s]
+}
+
+output "cluster_registration_tokens" {
+  description = "The k3s cluster registration token"
+  value       = rancher2_cluster.k3s[*].cluster_registration_token[0]
+}
---
+++ harvester-clusters/variables.tf
@@ -0,0 +1,78 @@
+variable "cluster_name" {
+  type        = string
+  default     = "load-testing"
+  description = "Unique identifier used in resource names and tags"
+}
+
+variable "cluster_count" {
+  type        = number
+  description = "Number of clusters to provision"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-west-1"
+}
+
+variable "security_groups" {
+  type        = list
+  default     = []
+  description = "A list of security group names (EC2-Classic) or IDs (default VPC) to associate with"
+}
+
+variable "server_instance_type" {
+  type        = string
+  description = "Instance type to use for k3s server"
+}
+
+variable "k3s_server_args" {
+  type        = string
+  default     = ""
+  description = "extra args to pass to k3s server"
+}
+
+variable "rancher_api_url" {
+  type        = string
+  description = "api url for rancher server"
+}
+
+variable "rancher_token_key" {
+  type        = string
+  description = "rancher server API token"
+}
+
+variable "ssh_keys" {
+  type        = list
+  default     = []
+  description = "SSH keys to inject into the EC2 instances"
+}
+
+variable "install_k3s_image" {
+  type        = string
+  default     = "v1.19.3-k3s1"
+  description = "k3s image to use during install (container image tag with the 'v')"
+}
+
+variable "k3d_version" {
+  type        = string
+  default     = "v3.4.0"
+  description = "k3d version to use during cluster create (release tag with the 'v')"
+}
+
+variable "k3s_cluster_secret" {
+  type        = string
+  default     = ""
+  description = "k3s cluster secret"
+}
+
+variable "insecure_flag" {
+  type        = bool
+  default     = false
+  description = "Flag used to determine if Rancher is using self-signed invalid certs (using a private CA)"
+}
+
+variable "cluster_labels" {
+  type        = map
+  default     = {}
+  description = "Labels to add to each provisioned cluster"
+}
